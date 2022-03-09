ebcc10ce391e233ca8dd1ab6fa32f33a3d64848d
Seeit180 <73988122+Anjal180@users.noreply.github.com>
2022-03-09 10:52:16 +0530

    Update admindash.php

--- admindash.php
+++ admindash.php
@@ -4,12 +4,16 @@ $msg="";
 ?>
 <?php
 include 'connection.php';
+
 if(isset($_POST['xslxdata'])){
 	 $file=$_FILES['doc']['tmp_name'];
+	
 	$ext=pathinfo($_FILES['doc']['name'],PATHINFO_EXTENSION);
 	if($ext=='xlsx'){
 		require('PHPExcel/PHPExcel.php');
 		require('PHPExcel/PHPExcel/IOFactory.php');
+		
+		
 		$obj=PHPExcel_IOFactory::load($file);
 		foreach($obj->getWorksheetIterator() as $sheet){
 			$getHighestRow=$sheet->getHighestRow();
@@ -17,11 +21,12 @@ if(isset($_POST['xslxdata'])){
 				 $custid=$sheet->getCellByColumnAndRow(0,$i)->getValue();
 				 $custname=$sheet->getCellByColumnAndRow(1,$i)->getValue();
 				 $prodid=$sheet->getCellByColumnAndRow(2,$i)->getValue();
-         $userid=$sheet->getCellByColumnAndRow(3,$i)->getValue();
-         $username=$sheet->getCellByColumnAndRow(4,$i)->getValue();
+        //  $userid=$sheet->getCellByColumnAndRow(3,$i)->getValue();
+        //  $username=$sheet->getCellByColumnAndRow(4,$i)->getValue();
+
 				if($custid!=''){
-					// mysqli_query($conn,"insert into excel(name,email,b_no) values('$name','$email','$b_no')");
-          mysqli_query($conn,"insert into prod_hdr(cust_id,cust_name,prod_id,user_id,user_name) values('$custid','$custname','$prodid','$userid','$username')");
+					// mysqli_query($conn,"insert into prod_hdr(,user_id,user_name) values(,'$userid','$username')");
+          mysqli_query($conn,"insert into prod_hdr(cust_id,cust_name,prod_id) values('$custid','$custname','$prodid')");
                     
 				}
 			}
@@ -46,7 +51,7 @@ if(isset($_POST['xslxdata'])){
 
 
 <h3 id="h3">Document Management <hr></h3>
-  <!-- <button type="button" id="btn" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Bulk Data Upload</button>
+  <button type="button" id="btn" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" >Bulk Data Upload</button>
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -96,41 +101,38 @@ if(isset($_POST['xslxdata'])){
    </div>
    
 </div>
-<br> -->
-<div class="container">  
-  <form id="myform">     
-    <table class="table table-striped" id="mytable">
-      <thead>
-        <tr class="table-primary">
-          <th ></th>
-          <th >File Category</th>
-          <th >Upload File</th>
-          <th >Filename</th>
-        </tr>
-      </thead>
-      <tbody id="tbody">
-        <tr>
-        <td></td>
-          <td>
-            
-            <select name="doc_type" id="cars" class="form-select ">
-              <option value="">choose</option>
-              <option value="saab">Saab</option>
-              <option value="opel">Opel</option>
-              <option value="audi">Audi</option>
-            </select>
-          </td>
-          <td><input type="file" name="doc" class="form-control"/></td>
-          <td><input type="text" name="filename" class="form-control"></td>
-          <th><button class="btn btn-primary"><i class="bi bi-pencil-fill"></i></button>
-          <button class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button>
-          <button class="btn btn-primary" onclick="addItem();"><i class="bi bi-plus-circle-fill"></i></button></th>
-        </tr>
-      </tbody>
-    </table>
-    <button class="btn btn-primary" onclick="addItem();" id="hide"><i class="bi bi-plus-circle-fill"></i></button></th>
+<br>
+<div class="container">       
+  <table class="table table-striped" id="tableS">
+    <thead>
+      <tr class="table-primary">
+        <th scope="col"></th>
+        <th scope="col">File Category</th>
+        <th scope="col">Upload File</th>
+        <th scope="col">Filename</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+      <td></td>
+        <td>
+          
+          <select name="cars" id="cars" class="form-select ">
+            <option value="">choose</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+        </td>
+        <td><input type="file" name="doc" class="form-control"/></td>
+        <td><input type="text" class="form-control"></td>
+        <td><button type="button" onclick="deleteRow(this);" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>
+        <td><button type="button" onclick="addItem();" class="btn btn-primary" ><i class="bi bi-plus-circle-fill"></i></button></td>
+      </tr>
+    </tbody>
+  </table>
+  <button type="button" class="btn btn-primary" onclick="addItem();" id="hide"><i class="bi bi-plus-circle-fill"></i></button>
     <input type="submit" name="submit" id="sumbit"class="btn btn-info" value="Upload">
-  </form> 
 </div>
 <div class="py-5"></div><div class="py-2"></div>
 <script type="text/javascript">
@@ -140,14 +142,12 @@ if(isset($_POST['xslxdata'])){
  
         var html = "<tr>";
             html += "<td></td>";
-            html += "<td><select name='doc_type' id='cars' class='form-select'><option value=''>choose</option><option value='saab'>Saab</option><option value='opel'>Opel</option><option value='audi'>Audi</option></select></td>";
-            html += "<td><input type='file' name='doc' class='form-control'/></td>";
-            html += "<td><input type='text' name='filename' class='form-control'></td>";
+           
             html += "<td><button type='button' onclick='deleteRow(this);'>Delete</button> <button type='button' onclick='addItem();'><i class='bi bi-plus-circle-fill'></button</td>"
-            
-        html += "</tr>";
+            html += "</tr>";
  
-        var row = document.getElementById("tbody").insertRow();
+        var r = document.getElementById("tbody");
+        var row= r.insertRow();
         row.innerHTML = html;
     }
  
@@ -155,11 +155,11 @@ function deleteRow(button) {
     button.parentElement.parentElement.remove();
     // first parentElement will be td and second will be tr.
 }
-$(document).ready(function(){
-    $('#hide').on('click',function(){
-        $('#hide').hide();
-    });
-});
+// $(document).ready(function(){
+//     $('#hide').on('click',function(){
+//         $('#hide').hide();
+//     });
+// });
 </script>
 <?php
 include 'footer.php';
