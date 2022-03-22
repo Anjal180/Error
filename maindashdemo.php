e50c66de6d3aa52b64f629207001c96c33c19781
Seeit180 <73988122+Anjal180@users.noreply.github.com>
2022-03-22 15:35:36 +0530

    upadated

--- maindashdemo.php
+++ maindashdemo.php
@@ -215,23 +215,23 @@ if(isset($_POST['upload'])){
     </thead>
     <tbody>
 
-      <tr>
+      <!-- <tr>
         <td></td>
         <td><select name="doctyp[]" class="form-select ">
         <option selected>choose</option>
-            <?php echo fill_select_box($conn); ?>
+            < ?php echo fill_select_box($conn); ?>
           </select>
         </td>
-        <td><input type="file" name="doc[]" id='inputfile' class="form-control"onChange='getoutput()'/></td>
-        <td><input type="text" class="outputfile" id="outputfile" name="filename[]"><input id="extension" type="text" name="extension" disabled></td>
+        <td><input type="file" name="doc[]" id='inputfile[]' data-id="1" class="form-control inputfile"/></td>
+        <td><input type="text" class="outputfile" id="outputfile1" name="filename[]"><input id="extension1" type="text" name="extension" disabled></td>
         <td><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>
-      </tr>
+      </tr> -->
       
     </tbody>
     <tfoot>
       <tr>
         <td colspan="4"></td>
-        <td><button type="button" id="add" class="btn btn-primary" ><i class="bi bi-plus-circle-fill"></i>Add</button>
+        <td><button type="button" id="add" data-id="32" class="btn btn-primary" ><i class="bi bi-plus-circle-fill"></i>Add</button>
         <input type="submit" name="upload" id="sumbit"class="btn btn-info" value="Upload"></td>
       </tr>
     </tfoot>
@@ -249,43 +249,43 @@ function getoutput() {
 }</script> -->
 
 
-<script>$(document).ready(function() {
+<script>
+var x=1;
+$(document).ready(function() {
+  
     var html = '<tr>';
     html += '<td></td>';
     html += '<td><select name="doctyp[]" class="form-select "><option >choose</option><?php echo fill_select_box($conn);?> </select></td > ';
-    html += '<td><input type="file" name="doc[]" id="inputfile" class="form-control" /></td>';
-    html += ' <td><input type="text"  id="outputfile" name="filename[]"><input id="extension" type="text" name="extension" disabled></td>';
+    html += '<td><input type="file" name="doc[]" id="inputfile[]" data-id='+x+' class="form-control inputfile"/></td>';
+    html += ' <td><input type="text" class="outputfile" id="outputfile'+x+'" name="filename[]"><input id="extension'+x+'" type="text" name="extension" disabled></td>';
     html += '<td><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>';
     html += '</tr>';
-    var x = 1;
-
+    // x++;
+    // var x = 2;
+   
     $("#add").click(function() {
-        $("#TableS").append(html);
+        $("#TableS").append(html);  
     });
     $('.table tbody').on('click', '#delete', function() {
         $(this).closest('tr').remove();
     });
+});
+// x++;
 
-      $(window).load(function() {
-        // alert("HELLO");
-        $("#inputfile").on('change', function (e) {
-        //   getoutput(this.value);
-          getoutput();
-        });
+// Using delegation.
+$(document).on('change', '.inputfile', function (e) {
+    var imgid = $(this).attr("id");
+    var filenames = [].slice.call(e.target.files).map(function (f) {
+        alert(f.name);
+        return f.name.split('.')[0];
     });
-
+    var Extnames = [].slice.call(e.target.files).map(function (g) {
+        return g.name.split('.')[1];
+    });
+    $('#extension' + $(this).attr("data-id")).val(Extnames);
+    $('#outputfile' + $(this).attr("data-id")).val(filenames);
 });
 
-
-function getFile(filePath) {
-    return filePath.substr(filePath.lastIndexOf('\\') + 1).split('.')[0];
-}
-
-function getoutput() {
-    // alert("hello");
-    outputfile.value = getFile(inputfile.value);
-      extension.value = inputfile.value.split('.')[1];
-}
 </script>
 <?php
 include 'footer.php';
