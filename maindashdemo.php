61c4fdbcce68c2e89cf507d1801b5ae8e5ce7c2d
Rahul <58558341+rahulps1000@users.noreply.github.com>
2022-03-22 18:19:44 +0530

    Update maindashdemo.php

--- maindashdemo.php
+++ maindashdemo.php
@@ -40,27 +40,27 @@ $msg="";
 <!------------------------ JQUERY SCRIPT FOR ADD ROW ----------------------->
 <!------------------------ VAR X=1 (ID FOR EACH FIELD BUT ITS NOT INCREASING) ----------------------->
 <script>
+function getHtml(index) {
+var html = '<tr>';
+    html += '<td></td>';
+    html += '<td><select name="doctyp[]" class="form-select "><option >choose</option></select></td > ';
+    html += '<td><input type="file" name="doc[]" id="inputfile[]" data-id='+index+' class="form-control inputfile"/></td>';
+    html += ' <td><input type="text" class="outputfile" id="outputfile'+index+'" name="filename[]"><input id="extension'+index+'" type="text" name="extension" disabled></td>';
+    html += '<td><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i>delete</button></td>';
+    html += '</tr>';
+    return html;
+}
 var x=1;
 $(document).ready(function() {
   
-    var html = '<tr>';
-    html += '<td></td>';
-    html += '<td><select name="doctyp[]" class="form-select "><option >choose</option></select></td > ';
-    html += '<td><input type="file" name="doc[]" id="inputfile[]" data-id='+x+' class="form-control inputfile"/></td>';
-    html += ' <td><input type="text" class="outputfile" id="outputfile'+x+'" name="filename[]"><input id="extension'+x+'" type="text" name="extension" disabled></td>';
-    html += '<td><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i>delete</button></td>';
-    html += '</tr>';
-    // x++;
-    // var x = 2;
-   
     $("#add").click(function() {
-        $("#TableS").append(html);  
+    		var html = getHtml(x++);
+        $("#TableS").append(html);
     });
     $('.table tbody').on('click', '#delete', function() {
         $(this).closest('tr').remove();
     });
 });
-// x++;
 
 <!------------------------ filename setting ----------------------->
 $(document).on('change', '.inputfile', function (e) {
