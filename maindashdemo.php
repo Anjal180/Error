cdb958c6a50fc54d256d7163a1baf2ee1856dc66
Seeit180 <73988122+Anjal180@users.noreply.github.com>
2022-03-30 11:01:01 +0530

    INSERTCODE ERROR

--- maindashdemo.php
+++ maindashdemo.php
@@ -9,44 +9,84 @@ $msg="";
 
 
 
-<!------------------------ TABLE----------------------->
+<?php
+function fill_select_box($conn){
+  $opt="";
+  $q2="SELECT * FROM doc_mst ORDER BY doc_type ASC";
+  $result2=mysqli_query($conn,$q2);
+  // $result2=$conn->query($query);
+  foreach($result2 as $row2)
+  {
+    $opt.='<option value="'.$row2['doc_type'].'">' .$row2['doc_type'].'</option>';
+  }
+  return $opt;
+}
 
-<div class="container">   
-  <form method="post" action="" enctype="multipart/form-data">    
-  <table class="table table-bordered" id="TableS">
+
+?>
+<!-------------------------------- PHP code for file upload ---------------------------------------->
+<?php 
+if(isset($_POST['upload'])){
+  extract($_POST);
+  $doctyp=$_POST['doctyp'];
+ $doc=$_FILES['doc']['name'];
+  // $filename=$_POST['filename'];
+  // $extension=$_POST['extension'];
+
+
+  foreach($doc as $key => $value){
+    echo $doc=$_FILES['doc']['name'][$key];
+   echo $file_type=pathinfo($doc,PATHINFO_EXTENSION);
+    echo $filePath='docfile/'.$doc;
+   echo $tmp_name=$_FILES['doc']['tmp_name'][$key];
+    move_uploaded_file($tmp_name,$filePath);
+    
+  // print_r($_POST);
+  // $extension=$_POST['extension'][$key];
+  // $filesnam=$filename.$extension;
+    
+   <!------------------ INSERT CODE DOCUMENTS UPLOAD---------------------------------->
+  $ins="INSERT INTO `up_doc_dtls`(`cust_id`, `prod_id`, `doc_type`, `file_name`, `doc_name`) VALUES ('$customer','$prodid','".$doctyp[$key]."','".$value."','".$filename[$key]."')";
+  $ab=mysqli_query($conn,$ins);
+  }
+}
+?>
+
+
+
+<div class="container">
+  
+  <!-- <form method="post" action="" enctype="multipart/form-data">   -->
+  <div class="table-wrapper-scroll-y my-custom-scrollbar"> 
+  <table class="table table-bordered table-striped mb-0" id="TableS">
     <thead>
       <tr class="table-primary">
         <th scope="col"></th>
         <th scope="col">File Category</th>
         <th scope="col">Upload File</th>
         <th scope="col">Filename</th>
-        <th></th>
+        <th scope="col"></th>
       </tr>
     </thead>
     <tbody>
-      
     </tbody>
-    <tfoot>
-      <tr>
-        <td colspan="4"></td>
-        <td><button type="button" id="add" data-id="32" class="btn btn-primary" ><i class="bi bi-plus-circle-fill"></i>Add</button>
-        <input type="submit" name="upload" id="sumbit"class="btn btn-info" value="Upload"></td>
-      </tr>
-    </tfoot>
   </table>
+  </div>
+    <button type="button" id="add" data-id="32" class="btn btn-primary" style="margin-left:85%;"><i class="bi bi-plus-circle-fill"></i></button>
+    <input type="submit" name="upload" id="sumbit"class="btn btn-info" value="Upload">
+    
   </form> 
-</div>
 
-<!------------------------ JQUERY SCRIPT FOR ADD ROW ----------------------->
-<!------------------------ VAR X=1 (ID FOR EACH FIELD BUT ITS NOT INCREASING) ----------------------->
+  </div>
+<div class="py-5"></div><div class="py-2"></div>
 <script>
 function getHtml(index) {
-var html = '<tr>';
-    html += '<td></td>';
-    html += '<td><select name="doctyp[]" class="form-select "><option >choose</option></select></td > ';
-    html += '<td><input type="file" name="doc[]" id="inputfile[]" data-id='+index+' class="form-control inputfile"/></td>';
-    html += ' <td><input type="text" class="outputfile" id="outputfile'+index+'" name="filename[]"><input id="extension'+index+'" type="text" name="extension" disabled></td>';
-    html += '<td><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i>delete</button></td>';
+var html = '<tr id="td">';
+    html += '<td id="td"></td>';
+    html += '<td id="td"><select name="doctyp[]" class="form-select"><option >choose</option><?php echo fill_select_box($conn); ?></select></td > ';
+    html += '<td id="td"><input type="file" name="doc[]" id="inputfile[]" data-id='+index+' class="form-control inputfile"/></td>';
+    html += ' <td id="td"><input type="text" class="form-control-sm outputfile" id="outputfile'+index+'" name="filename[]"><input id="extension'+index+'" type="text" class="form-control-sm" name="extension[]" disabled></td>';
+    html += '<td id="td"><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>';
     html += '</tr>';
     return html;
 }
@@ -56,17 +96,19 @@ $(document).ready(function() {
     $("#add").click(function() {
     		var html = getHtml(x++);
         $("#TableS").append(html);
+        $("table tbody #td").css('padding','3px');
+        $("table tbody .form-control-sm").css('border','1px solid #bdbdbd');
     });
     $('.table tbody').on('click', '#delete', function() {
         $(this).closest('tr').remove();
     });
 });
 
-<!------------------------ filename setting ----------------------->
+// <!------------------------ filename setting ----------------------->
 $(document).on('change', '.inputfile', function (e) {
     var imgid = $(this).attr("id");
     var filenames = [].slice.call(e.target.files).map(function (f) {
-        alert(f.name);
+        // alert(f.name);
         return f.name.split('.')[0];
     });
     var Extnames = [].slice.call(e.target.files).map(function (g) {
