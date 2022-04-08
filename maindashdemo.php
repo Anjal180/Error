7d762147d2d8a742935d788b48659a335acdf13a
Seeit180 <73988122+Anjal180@users.noreply.github.com>
2022-04-08 13:31:38 +0530

    Update maindashdemo.php

--- maindashdemo.php
+++ maindashdemo.php
@@ -1,124 +1,214 @@
 <?php
 include 'nav.php';
 include 'connection.php';
-$msg="";
 ?>
 
-<!------------------------------ Main Page Style Start ----------------------------->
-<h3 id="h3">Document Management <hr></h3>
+<!--------- style for btn and erorr -------------->
+<style>
+  #btn{
+    margin-left:78%;
+  }
+ hr{
+  margin: 0.5rem;
+ }
+ #h3 {
+    padding: 10px;
+}
+h3{
+  margin-bottom: 0rem;
+}
+  .error{
+		color: red;font-style: italic; font-weight: bold;
+    text-align: center;
+		}
+    .my-custom-scrollbar {
+    position: relative;
+    height: 335px;
+    overflow: auto;
+    }
+    .table-wrapper-scroll-y {
+    display: block;
+    }
+    #TableS thead th{
+      padding:5px;
+    position: sticky;
+    top: 0;
+    background-color:#d0e0fc;
+    }
+    table tbody #td{
+      padding: 3px;
+    } 
+  
+</style>
 
-
-
+<!----------------------------------- PHP code for Prod_hr table(id) ---------------------------------------->
 <?php
-function fill_select_box($conn){
-  $opt="";
-  $q2="SELECT * FROM doc_mst ORDER BY doc_type ASC";
-  $result2=mysqli_query($conn,$q2);
-  // $result2=$conn->query($query);
-  foreach($result2 as $row2)
-  {
-    $opt.='<option value="'.$row2['doc_type'].'">' .$row2['doc_type'].'</option>';
-  }
-  return $opt;
-}
+    $query   = "SELECT `prod_id` FROM prod_hdr";
+    $results = mysqli_query($conn, $query);
+  ?>
 
+  <script>    
+//  <!-------------------- RETRIVE DATA FROM DATABASE  -------------------------------->
+    function getAll(val){
+      var x=document.getElementById("product-id").value;
+      $.ajax({
+        type: "POST",
+        url: "getAll.php",
+        data:{
+          prod_id:x
+        },
+        success: function(data){
+          $("#ans").html(data);
+        }
+      });
+    }
+  </script>
 
-?>
-<!-------------------------------- PHP code for file upload ---------------------------------------->
-<?php 
-if(isset($_POST['upload'])){
-  extract($_POST);
-  $doctyp=$_POST['doctyp'];
- $doc=$_FILES['doc']['name'];
-  // $filename=$_POST['filename'];
-  // $extension=$_POST['extension'];
+<!-------------------- MAIN PAGE CODE  -------------------------------->
+<h3 id="h3">Document Management<hr></h3>
+<form method="post" action="" enctype="multipart/form-data">
 
+  <!----------------- Prodcut ID -------------------->
+  <div style="padding-left: 110px;">
+    <div class="form-row" >
+      <div class="col-lg-3">
+          <div class="input-group">
+            <label class="input-group-text">Product Id :</label>
+            <select  class="form-select" name="prodid" id="product-id" onChange="getAll(this.value);" required>
+              <option value="">Product Id</option>
+              <!----------------- Prodcut PHP CODE -------------------->
+              <?php
+                $sql = "SELECT * FROM prod_hdr GROUP BY prod_id ASC";
+                $res = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($res)) {
+                 
+              ?>
+              <option value="<?php  echo $row['prod_id']; ?>"><?php echo $row['prod_id']; ?></option>
+              <?php 
+                
+                }
+              ?>
+            </select>
+          </div>
+        </div>
+    </div> 
+  </div><br>
 
-  foreach($doc as $key => $value){
-    echo $doc=$_FILES['doc']['name'][$key];
-   echo $file_type=pathinfo($doc,PATHINFO_EXTENSION);
-    echo $filePath='docfile/'.$doc;
-   echo $tmp_name=$_FILES['doc']['tmp_name'][$key];
-    move_uploaded_file($tmp_name,$filePath);
-    
-  // print_r($_POST);
-  // $extension=$_POST['extension'][$key];
-  // $filesnam=$filename.$extension;
-    
-   <!------------------ INSERT CODE DOCUMENTS UPLOAD---------------------------------->
-  $ins="INSERT INTO `up_doc_dtls`(`cust_id`, `prod_id`, `doc_type`, `file_name`, `doc_name`) VALUES ('$customer','$prodid','".$doctyp[$key]."','".$value."','".$filename[$key]."')";
-  $ab=mysqli_query($conn,$ins);
-  }
-}
-?>
+  <!-------------------------------- PHP code for file upload ---------------------------------------->
+  <?php 
+    if(isset($_POST['upload'])){
+      extract($_POST);
+      $IDDir=$_POST["prodid"];
+      $doctyp=$_POST['doctyp'];
+      $doc=$_FILES['doc']['name'];
+      
+      // ------------Create folder in F Drive----------
+      foreach($doc as $key => $value){
+        $name = $_POST["filename"][$key];
+        $doc=$_FILES['doc']['name'][$key];
+        $extension=pathinfo($doc,PATHINFO_EXTENSION);
+        $filePath='F:/Root/'.$IDDir;
+        if(!is_dir($filePath)){
+          mkdir($filePath, 0755);
+      }
+      
+      $filePath .= "/". $name.".".$extension;
+        $tmp_name=$_FILES['doc']['tmp_name'][$key];
+        move_uploaded_file($tmp_name,$filePath);
+        
+        //<!-------------------- INSERT QUERY FOR MAIN PAGE  -------------------------------->
+        $ins="INSERT INTO `up_doc_dtls`( `prod_id`, `doc_type`, `file_name`, `doc_name`, `ext`) 
+        VALUES ('$prodid','".$doctyp[$key]."','".$value."','".$filename[$key]."','".$ext[$key]."')";
+        $ab=mysqli_query($conn,$ins);
+      }
+    }
+  ?>
 
-
-
-<div class="container">
-  
-  <!-- <form method="post" action="" enctype="multipart/form-data">   -->
-  <div class="table-wrapper-scroll-y my-custom-scrollbar"> 
-  <table class="table table-bordered table-striped mb-0" id="TableS">
-    <thead>
-      <tr class="table-primary">
-        <th scope="col"></th>
-        <th scope="col">File Category</th>
-        <th scope="col">Upload File</th>
-        <th scope="col">Filename</th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-  </div>
-    <button type="button" id="add" data-id="32" class="btn btn-primary" style="margin-left:85%;"><i class="bi bi-plus-circle-fill"></i></button>
+  <!-------------------- TABLE  -------------------------------->
+  <div class="container">
+    <div class="table-wrapper-scroll-y my-custom-scrollbar"> 
+      <table class="table  table-striped mb-0" id="TableS">
+        <thead>
+          <tr class="table-primary">
+            <!-- <th scope="col"></th> -->
+            <th scope="col">File Category</th>
+            <th scope="col">Upload File</th>
+            <th scope="col">Filename</th>
+            <th scope="col"> </th>
+          </tr>
+        </thead>
+        <tbody id="ans">
+        </tbody>
+      </table>
+    </div>
+    <hr>
+    <button type="button" id="add" data-id="32" class="btn btn-primary" style="margin-left:75%;"><i class="bi bi-plus-circle-fill"></i></button>
     <input type="submit" name="upload" id="sumbit"class="btn btn-info" value="Upload">
-    
-  </form> 
-
   </div>
-<div class="py-5"></div><div class="py-2"></div>
+</form>
+<!-------------------- FROM END  -------------------------------->
+<div class="py-2"></div>
+
+<!-------------------- ADDING ROWS FOR TABLE html += '<td id="td"></td>'; -------------------------------->
 <script>
-function getHtml(index) {
-var html = '<tr id="td">';
-    html += '<td id="td"></td>';
-    html += '<td id="td"><select name="doctyp[]" class="form-select"><option >choose</option><?php echo fill_select_box($conn); ?></select></td > ';
-    html += '<td id="td"><input type="file" name="doc[]" id="inputfile[]" data-id='+index+' class="form-control inputfile"/></td>';
-    html += ' <td id="td"><input type="text" class="form-control-sm outputfile" id="outputfile'+index+'" name="filename[]"><input id="extension'+index+'" type="text" class="form-control-sm" name="extension[]" disabled></td>';
-    html += '<td id="td"><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>';
-    html += '</tr>';
-    return html;
-}
-var x=1;
-$(document).ready(function() {
-  
+  function getHtml(index) {
+  var html = '<tr id="td">';
+      
+      html += '<td id="td" class="col"><input type="text" name="doctyp[]" value="General" class="form-control-sm" readonly></td> ';
+      html += '<td id="td" class="col"><input type="file" name="doc[]" id="inputfile[]" data-id='+index+' class="form-control-sm inputfile" required/></td>';
+      html += ' <td id="td" class="col"><input type="text" class="form-control-sm outputfile" id="outputfile'+index+'" name="filename[]" autocomplete="off" required><input id="extension'
+      +index+'"  name="ext[]" class="form-control-sm" readonly></td>';
+      html += '<td id="td"class="col"><button type="button" id="delete" class="btn btn-primary"><i class="bi bi-trash3-fill"></i></button></td>';
+      html += '</tr>';
+      return html;
+  }
+  var x=1;
+
+  // <!-------------------- ADD,REMOVE IN TABLE  -------------------------------->
+  $(document).ready(function() {
     $("#add").click(function() {
-    		var html = getHtml(x++);
-        $("#TableS").append(html);
-        $("table tbody #td").css('padding','3px');
-        $("table tbody .form-control-sm").css('border','1px solid #bdbdbd');
+      var html = getHtml(x++);
+      $("#TableS").append(html);
+      $("table tbody .form-control-sm").css('border','1px solid #bdbdbd');
     });
     $('.table tbody').on('click', '#delete', function() {
-        $(this).closest('tr').remove();
+      $(this).closest('tr').remove();
     });
-});
-
-// <!------------------------ filename setting ----------------------->
-$(document).on('change', '.inputfile', function (e) {
-    var imgid = $(this).attr("id");
-    var filenames = [].slice.call(e.target.files).map(function (f) {
-        // alert(f.name);
-        return f.name.split('.')[0];
+  });
+  
+  // <!------------------------ filename setting ----------------------->
+  $(document).on('change', '.inputfile', function (e) {
+      var imgid = $(this).attr("id");
+      var filenames = [].slice.call(e.target.files).map(function (f) {
+      // alert(f.name);
+      return f.name.split('.')[0];
     });
     var Extnames = [].slice.call(e.target.files).map(function (g) {
-        return g.name.split('.')[1];
+      return g.name.split('.')[1];
     });
-    $('#extension' + $(this).attr("data-id")).val(Extnames);
-    $('#outputfile' + $(this).attr("data-id")).val(filenames);
-});
-
+        $('#extension' + $(this).attr("data-id")).val(Extnames);
+        $('#outputfile' + $(this).attr("data-id")).val(filenames);
+  });
 </script>
+
+<!--  Delete Ajax Code   -->
+
+<script>
+	  function deleteAjax(file_id){
+      $(document).ready(function(){
+        $.ajax({
+          type:'post',
+          url:'delete.php',
+          data:{file_id:file_id},
+          success:function(data){
+            alert("deleted");
+            // getAll(val);
+          }
+        })
+      })
+    }
+</script>
+
 <?php
 include 'footer.php';
 ?> 
